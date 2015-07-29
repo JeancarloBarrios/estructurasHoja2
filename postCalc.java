da311b028ec75f3e678a608f3266e03082b9c8c4
derekolsson <derekolsson@gmail.com>
2015-07-29 07:41:38 -0600

    StackVector
    Finished StackVector

--- postCalc.java
+++ postCalc.java
@@ -3,36 +3,22 @@
  */
 package hoja2;
 
-/**
- * @author venator1
- *
- */
+
 public class postCalc implements Calculadora {
 
-	/* (non-Javadoc)
-	 * @see hoja2.Calculadora#setString(java.lang.String)
-	 */
 	@Override
 	public void setString(String linea) {
-		// TODO Auto-generated method stub
 
 	}
 
-	/* (non-Javadoc)
-	 * @see hoja2.Calculadora#calcularVector()
-	 */
 	@Override
 	public boolean calcularVector() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	/* (non-Javadoc)
-	 * @see hoja2.Calculadora#getResultado()
-	 */
 	@Override
 	public int getResultado() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
